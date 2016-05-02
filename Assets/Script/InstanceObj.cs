223e89e647298a034d3b1f3a6672e0d19e017b6c
haratatsurou <hara.tatsurou@gmail.com>
2016-05-02 11:52:26 +0900

    オブジェクトをクリックしたら追従するように

--- Assets/Script/InstanceObj.cs
+++ Assets/Script/InstanceObj.cs
@@ -5,29 +5,31 @@ using UniRx.Triggers;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class InstanceObj : MonoBehaviour,IBeginDragHandler,IDragHandler ,IEndDragHandler{
+public class InstanceObj : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
     public GameObject insobj;
     private GameObject createobj;
-	void Start () {
-	
-	}
-	
-	void Update () {
-	
-	}
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
     public void OnBeginDrag(PointerEventData obj) {
-        Vector3 localpos = Vector3.zero;
-        RectTransformUtility.ScreenPointToWorldPointInRectangle(GetComponent<RectTransform>( ) , obj.position , Camera.main , out localpos);
-        Vector2 hoge = localpos;
-        createobj = Instantiate(insobj , hoge , insobj.transform.rotation) as GameObject;
+        createobj = Instantiate(insobj , fromScreenPostoWorldPos(obj) , insobj.transform.rotation) as GameObject;
+        createobj.GetComponent<BoxCollider>( ).enabled = false;
     }
     public void OnDrag(PointerEventData obj) {
-        Vector3 localpos = Vector3.zero;
-        RectTransformUtility.ScreenPointToWorldPointInRectangle(GetComponent<RectTransform>( ) , obj.position , Camera.main , out localpos);
-        Vector2 hoge = localpos;
-        createobj.transform.position = hoge ;
+        createobj.transform.position = fromScreenPostoWorldPos(obj);
     }
     public void OnEndDrag(PointerEventData obj) {
+        createobj.GetComponent<BoxCollider>( ).enabled = true;
+    }
+    Vector2 fromScreenPostoWorldPos(PointerEventData ped) {
+        Vector3 localpos = Vector3.zero;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(GetComponent<RectTransform>( ) , ped.position , Camera.main , out localpos);
+        Vector2 hoge = localpos;
+        return hoge;
 
     }
 
---
+++ Assets/operation.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class operation : MonoBehaviour{
+    //public void OnBeginDrag(PointerEventData ped) {
+    //    //GetComponent<BoxCollider>( ).enabled = false;
+    //    this.gameObject.transform.position = fromScreenPostoWorldPos(ped);
+    //}
+    //public void OnDrag(PointerEventData ped) {
+    //    this.gameObject.transform.position = fromScreenPostoWorldPos(ped);
+    //}
+    //public void OnEndDrag(PointerEventData ped) {
+
+    //}
+    //public void OnPointerClick(PointerEventData ped) {
+    //    print("角度変える何か");
+    //}
+    //Vector2 fromScreenPostoWorldPos(PointerEventData ped) {
+    //    Vector3 localpos = Vector3.zero;
+    //    RectTransform setui = GameObject.Find("UI").GetComponent<RectTransform>( );
+    //    RectTransformUtility.ScreenPointToWorldPointInRectangle(setui, ped.position , Camera.main , out localpos);
+    //    Vector2 hoge = localpos;
+    //    print(hoge);
+    //    return hoge;
+    //}
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown() {
+        this.screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        this.offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x , Input.mousePosition.y , screenPoint.z));
+        this.gameObject.GetComponent<BoxCollider>( ).isTrigger = true;
+    }
+
+    void OnMouseDrag() {
+        Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x , Input.mousePosition.y , screenPoint.z);
+        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + this.offset;
+        transform.position = currentPosition;
+    }
+    void OnMouseUp() {
+        this.gameObject.GetComponent<BoxCollider>( ).isTrigger = false;
+
+    }
+
+}
