741225a57d97ed51824a6d105539a2a979cf4f8d
haratatsurou <hara.tatsurou@gmail.com>
2016-06-25 13:58:23 +0900

    ジョインと部分の微調整

--- Assets/Script/turndisplay.cs
+++ Assets/Script/turndisplay.cs
@@ -25,10 +25,8 @@ public class turndisplay : MonoBehaviour {
         //ジョインとの変更------------------------------------------------------------------------------------
         var underblock = GameObject.Find("underblock").GetComponent<HingeJoint>( );
         underblock.axis = new Vector3(0 , 0 , -90f);
-        underblock.anchor = new Vector3(underblock.anchor.x , -underblock.anchor.y , underblock.anchor.z);
         var topblock = GameObject.Find("topblock").GetComponent<HingeJoint>( );
         topblock.axis= new Vector3(0 , 0 , -90f);
-        topblock.anchor = new Vector3(topblock.anchor.x , -topblock.anchor.y , topblock.anchor.z);
         //-------------------------------------------------------------------------------------------------
         GameObject.Find("UI/Return").GetComponent<Button>( ).interactable = false;
         GameObject.Find("UI/reset").GetComponent<Button>( ).interactable = true;
